4c14bbcc36a57c22449c0631e07153fcab0eb6b3
andresverjan <51174913+andresverjan@users.noreply.github.com>
2020-05-07 13:30:52 -0500

    add Id for each product

--- models/producto.js
+++ models/producto.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 const productoSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
    name: {
         type: String,
         required: true
