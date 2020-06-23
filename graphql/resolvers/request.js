b2dc164545d9e6598dd68227c22c64babc054621
andresverjan <51174913+andresverjan@users.noreply.github.com>
2020-06-22 22:09:33 -0500

    GetMyDetails DOne! 90%

--- graphql/resolvers/request.js
+++ graphql/resolvers/request.js
@@ -1,6 +1,7 @@
 const Request = require('../../models/request')
 const RequestDetail = require('../../models/requestdetails')
 
+
 module.exports = {
     requests: async args => {
         try {
@@ -9,121 +10,106 @@ module.exports = {
                 return {
                     ...item._doc,
                     _id: item.id,
-                    createdAt: item._doc.createdAt?new Date(item._doc.createdAt).toISOString():new Date().toISOString(),
-                    updatedAt: item._doc.updatedAt?new Date(item._doc.updatedAt).toISOString():new Date().toISOString()
-                } 
-            })           
+                    createdAt: item._doc.createdAt ? new Date(item._doc.createdAt).toISOString() : new Date().toISOString(),
+                    updatedAt: item._doc.updatedAt ? new Date(item._doc.updatedAt).toISOString() : new Date().toISOString()
+                }
+            })
         }
         catch (error) {
             throw error
         }
     },
-    
+
     getDetailByRequestId: async args => {
         try {
-          console.log("argumentos ");
-          console.log(args);
-          const  requestId = args.requestId;
-          console.log("el  valor es: " + requestId);
-          const list = await Request.find( { _id: { $eq: requestId}});
-          const details = await RequestDetail.find( { requestId: { $eq: requestId}});          
-          if (!list) {              
-            throw new Error('not found');
-          }
-          return list.map(item => {
-            return {
-                ...item._doc,            
-                _id: item.id,
-                details: details,
-                createdAt: item._doc.createdAt?new Date(item._doc.createdAt).toISOString():new Date().toISOString(),
-                updatedAt: item._doc.updatedAt?new Date(item._doc.updatedAt).toISOString():new Date().toISOString()
+            console.log("argumentos ");
+            console.log(args);
+            const requestId = args.requestId;
+            console.log("el  valor es: " + requestId);
+            const list = await Request.find({ _id: { $eq: requestId } });
+            const details = await RequestDetail.find({ requestId: { $eq: requestId } });
+            if (!list) {
+                throw new Error('not found');
             }
-        })
+            return list.map(item => {
+                return {
+                    ...item._doc,
+                    _id: item.id,
+                    details: details,
+                    createdAt: item._doc.createdAt ? new Date(item._doc.createdAt).toISOString() : new Date().toISOString(),
+                    updatedAt: item._doc.updatedAt ? new Date(item._doc.updatedAt).toISOString() : new Date().toISOString()
+                }
+            })
         }
         catch (error) {
             throw error
         }
-       },
+    },
 
-    getMyRequest: async args =>
-    {
+    getMyRequest: async args => {
         try {
             console.log("argumentos ");
             console.log(args);
-            const  userId = args.userId;
+            const userId = args.userId;
             console.log("el  userId es: " + userId);
 
-           const joinTable = await Request
-           .aggregate(
-            [
-                { 
-                    "$project" : {
-                        "request" : "$$ROOT",
-                        "userId": 1,
-                        "total":1,
-                    }
-                }, 
-                { 
-                    "$lookup" : { 
-                        "localField" : "request._id", 
-                        "from" : "requestdetails", 
-                        "foreignField" : "requestId", 
-                        "as" : "details"
-                    }
-                }, 
-                { 
-                    "$unwind" : { 
-                        "path" : "$details", 
-                        "preserveNullAndEmptyArrays" : false
-                    }
-                }, 
-                { 
-                    "$lookup" : { 
-                        "localField" : "details.subproductoId", 
-                        "from" : "subproducts", 
-                        "foreignField" : "_id", 
-                        "as" : "subproducts"
-                    }
-                }, 
-                { 
-                    "$unwind" : { 
-                        "path" : "$subproducts", 
-                        "preserveNullAndEmptyArrays" : false
-                    }
-                }, 
-                { 
-                    "$match" : { 
-                        "request.userId" : "5ebdc068a9f7740017c79d1f"
-                    }
-                }
-            ]
-        );
-            
+            const joinTable = await Request
+                .aggregate(
+                    [
+                        {
+                            "$lookup": {
+                                "from": "requestdetails",
+                                "let": { "request_id": "$_id" },
+                                "pipeline": [
+                                    { "$match": { "$expr": { "$eq": ["$requestId", "$$request_id"] } } },
+                                    {
+                                        "$lookup": {
+                                            "from": "subproducts",
+                                            "let": { "subproducto_id": "$subproductoId" },
+                                            "pipeline": [
+                                                { "$match": { "$expr": { "$eq": ["$_id", "$$subproducto_id"] } } }
+                                            ],
+                                            "as": "subproducto"
+                                        }
+                                    }
+                                ],
+                                "as": "details"
+                            }
+                        },
+                        {
+                            "$match": {
+                                "userId": "5eef64986e486e961799744f"
+                            }
+                        }
+                    ]);
             console.log("**********************");
-            //console.log(joinTable);
-            console.log("**********************");            
-           
+            console.log(joinTable);
+            console.log("**********************");
             //const details = await RequestDetail.find( { requestId: { $eq: requestId}});
-
             if (!joinTable) {
-              throw new Error('not found');
+                throw new Error('not found');
             }
             return joinTable.map(item => {
-                console.log(item);
-              return {
-                  ...item._doc,            
-                  _id: item.id,
-                  total : item.request.total,
-                  userId: item.request.userId,
-                  createdAt: item.request.createdAt,
-                  details : [item.details],
-                  subproducto : item.subproducts
-              }
-          })
-          }
-          catch (error) {
-              throw error
-          }
+                return {
+                    ...item._doc,
+                    _id: item.id, 
+                    userId: item.userId,
+                    total : item.total,
+                    details: item.details.map(detalle=> {
+                        return  {                             
+                            ...detalle.doc,
+                            _id           : detalle.id,
+                            requestId     : detalle.requestId,                            
+                            subproductoId : detalle.subproductoId,
+                            subproducto   : detalle.subproducto[0]
+                        }
+                    })
+                }
+            })
+        }
+        catch (error) {
+            throw error
+        }
     },
 
     createRequest: async args => {
@@ -131,25 +117,25 @@ module.exports = {
             const { userId, latlng, total, details } = args.request
             console.group(details);
 
-            const request = new Request({                            
+            const request = new Request({
                 userId,
                 latlng,
                 total,
                 createdAt: new Date().toISOString(),
                 updatedAt: new Date().toISOString()
             });
-            
+
             const newObj = await request.save();
-            let newDetail= details.map(addition => {
+            let newDetail = details.map(addition => {
                 return {
                     ...addition,
                     requestId: newObj._id,
                     createdAt: new Date().toISOString(),
                     updatedAt: new Date().toISOString()
-                } 
+                }
             });
             console.log(newDetail);
-            responseDetails= RequestDetail.insertMany(newDetail);
+            responseDetails = RequestDetail.insertMany(newDetail);
             console.log(responseDetails);
             return { ...newObj._doc, _id: newObj.id }
         }
