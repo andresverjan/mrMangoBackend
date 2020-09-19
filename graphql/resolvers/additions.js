ac2139f5661a547d0a4220e0ffb2df756629f2ad
andresverjan <andresverjan@hotmail.com>
2020-09-19 01:42:12 -0500

    Create, update, Delete Additions
    Create, update, Delete Additions

--- graphql/resolvers/additions.js
+++ graphql/resolvers/additions.js
@@ -17,8 +17,65 @@ module.exports = {
         return {
           ...item._doc,
           _id: item._id,
+          active: item.active ? item.active : true,
         };
       });
+    } catch (error) {
+      throw error;
+    }
+  },
+  createAddition: async (args) => {
+    try {
+      const { name, img, value, active, description } = args.addition;
+      console.log(args);
+      const item = new Additions({
+        name,
+        img,
+        value,
+        active,
+        description,
+      });
+      const newItem = await item.save();
+      return { ...newItem._doc, _id: newItem._id };
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  updateAddition: async (args) => {
+    try {
+      const { _id, name, img, value, active, description } = args.addition;
+
+      const objToUpdate = new Additions({
+        _id,
+        name,
+        img,
+        value,
+        active,
+        description,
+      });
+      const newUser = await Additions.findOneAndUpdate(
+        objToUpdate._id,
+        objToUpdate
+      );
+      if (!newUser) {
+        throw new Error("Not found");
+      }
+      return { ...newUser._doc, _id: newUser._id };
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  deleteAddition: async (args) => {
+    try {
+      const { _id } = args.addition;
+      console.log(args);
+      const item = new Additions({
+        _id: _id,
+      });
+      const newUser = await item.deleteOne(item._id);
+      return { ...newUser._doc, _id: newUser._id };
     } catch (error) {
       throw error;
     }
