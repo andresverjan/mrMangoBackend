5a1ca516542386f968fd0daa44b5833742ec8f59
andresverjan <andresverjan@hotmail.com>
2020-09-19 01:04:48 -0500

    Filtro para Adiciones.

--- graphql/resolvers/additions.js
+++ graphql/resolvers/additions.js
@@ -1,19 +1,26 @@
-const Additions = require('../../models/additions')
+const Additions = require("../../models/additions");
+const helpers = require("../../helpers");
+
 module.exports = {
-    additions: async args => {
-        console.log("argumentos ");
-        console.log(args);
+  additions: async (args) => {
+    console.log("argumentos ");
+    console.log(args);
     try {
-       const list = await Additions.find()
-        return list.map(item => {
-            return {
-                ...item._doc,
-                _id: item.id}
-        })
+      let where = {};
+      if (args.filter != null && args.filter != undefined) {
+        let filter = helpers.getFilterFormObject(args.filter);
+        where = { $or: filter };
+      }
+
+      const list = await Additions.find(where);
+      return list.map((item) => {
+        return {
+          ...item._doc,
+          _id: item._id,
+        };
+      });
+    } catch (error) {
+      throw error;
     }
-    catch (error) {
-        throw error
-    }
- }
- 
-}
+  },
+};
