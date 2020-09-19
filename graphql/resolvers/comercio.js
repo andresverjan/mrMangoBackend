d9619647a4a21b2b5356ab14c04727bf710dc47b
andresverjan <andresverjan@hotmail.com>
2020-09-19 00:46:43 -0500

    SE hace funcionaldiad de Filtrado por cualquier propiedad de un Objeto
    FilterBy

--- graphql/resolvers/comercio.js
+++ graphql/resolvers/comercio.js
@@ -1,105 +1,108 @@
-const Comercio = require('../../models/comercios');
-const User = require('../../models/user');
-const helpers = require('../../helpers');
+const Comercio = require("../../models/comercios");
+const User = require("../../models/user");
+const helpers = require("../../helpers");
 
 module.exports = {
-
-    getComercioByLocation: async ({location}, ctx) => {
-        try {
-            const list = await Comercio.find();
-            const {lat, lng, id} = location;
-            userCoord = {
-                lat,
-                lng,
-                id
-            }
-            
-            const nearestShop = helpers.getNearestShop(list, userCoord);
-            console.log(nearestShop);
-            let userData = await helpers.getUserByJwt(ctx);
-
-            //Comprobar si el ID del usuario y su JWT concuerdan
-             if (!userData.id === id) {
-                 throw new Error('ID del request y del usuario no concuerdan');
-             }
-
-            userData.latlng = `${lat},${lng}`;
-            userData.comercioId = nearestShop.id;
-
-            await User.findOneAndUpdate({_id:userData.id}, { $set: userData },  {new: true} );
-
-            return nearestShop;
-        } catch (error) {
-            console.log(error);
-        }
-    },
-
-    comercios: async () => {
+  getComercioByLocation: async ({ location }, ctx) => {
     try {
-       const list = await Comercio.find()
-        return list.map(item => {
-            return {
-                ...item._doc
-            }
-        })
-    }
-    catch (error) {
-        throw error
-    }
- },
+      const list = await Comercio.find();
+      const { lat, lng, id } = location;
+      userCoord = {
+        lat,
+        lng,
+        id,
+      };
 
-  createComercio: async args => {
-  try {
-    const { id, name, location,  description  } = args.comercio
-    console.log(args);
-    const item = new Comercio({
+      const nearestShop = helpers.getNearestShop(list, userCoord);
+      console.log(nearestShop);
+      let userData = await helpers.getUserByJwt(ctx);
+
+      //Comprobar si el ID del usuario y su JWT concuerdan
+      if (!userData.id === id) {
+        throw new Error("ID del request y del usuario no concuerdan");
+      }
+
+      userData.latlng = `${lat},${lng}`;
+      userData.comercioId = nearestShop.id;
+
+      await User.findOneAndUpdate(
+        { _id: userData.id },
+        { $set: userData },
+        { new: true }
+      );
+
+      return nearestShop;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  comercios: async (args) => {
+    try {
+      let where = {};
+      if (args.filter != null && args.filter != undefined) {
+        let filter = helpers.getFilterFormObject(args.filter);
+        where = { $or: filter };
+      }
+      const list = await Comercio.find(where);
+      return list.map((item) => {
+        return {
+          ...item._doc,
+        };
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  createComercio: async (args) => {
+    try {
+      const { id, name, location, description } = args.comercio;
+      console.log(args);
+      const item = new Comercio({
         id,
         name,
         location,
-        description
-    })
-    const newItem = await item.save();
-    return { ...newItem._doc, _id: newItem.id }
-  }
-  catch (error) {
-      throw error
-  }
- }, 
+        description,
+      });
+      const newItem = await item.save();
+      return { ...newItem._doc, _id: newItem.id };
+    } catch (error) {
+      throw error;
+    }
+  },
 
- updateComercio: async args => {
+  updateComercio: async (args) => {
     try {
-      const { _id , name, location, description } = args.comercio
-      
-     const objToUpdate = new Comercio({
-         _id,
-          name,
-          location, 
-          description
-      })
-      const newUser = await User.findOneAndUpdate(objToUpdate._id,  objToUpdate );
+      const { _id, name, location, description } = args.comercio;
+
+      const objToUpdate = new Comercio({
+        _id,
+        name,
+        location,
+        description,
+      });
+      const newUser = await User.findOneAndUpdate(objToUpdate._id, objToUpdate);
       if (!newUser) {
-        throw new Error('Not found');
+        throw new Error("Not found");
       }
-      return { ...newUser._doc, _id: newUser.id }
+      return { ...newUser._doc, _id: newUser.id };
+    } catch (error) {
+      throw error;
     }
-    catch (error) {
-        throw error
-    }
-   } , 
+  },
 
-   deleteComercio: async args => {
+  deleteComercio: async (args) => {
     try {
-      const { _id} = args.comercio
+      const { _id } = args.comercio;
       console.log(args);
-      const item = new Comercio({ 
-          _id: _id
-      })
+      const item = new Comercio({
+        _id: _id,
+      });
       const newUser = await item.deleteOne(item._id);
-      return { ...newUser._doc, _id: newUser.id }
+      return { ...newUser._doc, _id: newUser.id };
+    } catch (error) {
+      throw error;
     }
-    catch (error) {
-        throw error
-    }
-   } 
-
-}
+  },
+};
--- helpers.js
+++ helpers.js
@@ -1,54 +1,74 @@
-const User = require('./models/user');
+const User = require("./models/user");
 
 const getCurrentDateTime = () => {
-    const d = new Date();
-    const date = d.toISOString().split('T')[0];
-    const time = d.toTimeString().split(' ')[0];
-    return `${date} ${time}`;
-}
+  const d = new Date();
+  const date = d.toISOString().split("T")[0];
+  const time = d.toTimeString().split(" ")[0];
+  return `${date} ${time}`;
+};
 
 const getNearestShop = (arrComercios, userCoord) => {
-    const EARTH_RADIUS = 6371;
-    let minDistance = Number.MAX_SAFE_INTEGER;
-    let nearestComercio = arrComercios[0];
+  const EARTH_RADIUS = 6371;
+  let minDistance = Number.MAX_SAFE_INTEGER;
+  let nearestComercio = arrComercios[0];
 
-    arrComercios.forEach((comercio) => {
+  arrComercios.forEach((comercio) => {
+    const userLat = userCoord.lat;
+    const userLng = userCoord.lng;
+    const shopLat = comercio.lat;
+    const shopLng = comercio.lng;
 
-        const userLat = userCoord.lat;
-        const userLng = userCoord.lng;
-        const shopLat = comercio.lat;
-        const shopLng = comercio.lng;
+    const deltLat = Number(shopLat) - Number(userLat);
+    const deltLng = Number(shopLng) - Number(userLng);
 
-        const deltLat = Number(shopLat) - Number(userLat);
-        const deltLng = Number(shopLng) - Number(userLng);
+    const a =
+      Math.pow(Math.sin(deltLat / 2), 2) +
+      Math.cos(Number(userLat)) *
+        Math.cos(Number(shopLat)) *
+        Math.pow(Math.sin(deltLng / 2), 2);
 
-        const a = Math.pow(Math.sin(deltLat / 2), 2) + Math.cos(Number(userLat)) * Math.cos(Number(shopLat)) * Math.pow(Math.sin(deltLng / 2), 2)
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const dist = EARTH_RADIUS * c;
 
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const dist = EARTH_RADIUS * c;
+    if (dist < minDistance) {
+      minDistance = dist;
+      nearestComercio = comercio;
+    }
+  });
 
-        if (dist < minDistance) {
-            minDistance = dist;
-            nearestComercio = comercio;
-        }
-    });
-
-    return nearestComercio;
-}
+  return nearestComercio;
+};
 
 const getUserByJwt = async (ctx) => {
-    let token = ctx.headers.authorization;
-    token = token.replace('Bearer ', '');
-    let userData;
-    try {
-        userData = await User.findOne({ jwt: token });
-    } catch (error) {
-        console.log(error);
-        throw new Error('Usuario no encontrado');
+  let token = ctx.headers.authorization;
+  token = token.replace("Bearer ", "");
+  let userData;
+  try {
+    userData = await User.findOne({ jwt: token });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Usuario no encontrado");
+  }
+
+  return userData;
+};
+
+const getFilterFormObject = (object) => {
+  let result = [];
+  for (let key of Object.keys(object)) {
+    let valor = object[key];
+    if (valor != null && valor != "" && valor != undefined) {
+      var obj = {};
+      obj[key] = { $regex: valor, $options: "i" };
+      result.push(obj);
     }
+  }
+  return result;
+};
 
-    return userData;
-}
-
-
-module.exports = { getNearestShop, getUserByJwt, getCurrentDateTime }
+module.exports = {
+  getNearestShop,
+  getUserByJwt,
+  getCurrentDateTime,
+  getFilterFormObject,
+};
