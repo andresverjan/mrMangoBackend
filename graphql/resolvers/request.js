ac837b8b5398ff8f21ebe9c11b9e836007749138
andresverjan <51174913+andresverjan@users.noreply.github.com>
2020-10-20 21:12:22 -0500

    Revert "Merge pull request #8 from andresverjan/jamir-develop"
    This reverts commit 2267406bb7411ff4bba2d5a1887a5de89702bbd5, reversing
    changes made to 7f3f8e718f57247df92cf0d0dd2e8d05cec06323.

--- graphql/resolvers/request.js
+++ graphql/resolvers/request.js
@@ -1,184 +1,165 @@
+
 const Request = require("../../models/request");
+const RequestDetail = require('../../models/requestdetails');
+const RequestDetailsAdditions = require('../../models/requestDetailsAdditions');
 const RequestObs = require("../../models/requestObs");
+const Subproducts = require("../../models/subproducts");
+const helpers = require("../../helpers");
 
 module.exports = {
-  requests: async (args) => {
-    try {
-      const requestList = Request.find()
-        .populate({ path: "details.subproducto", model: "subproducts" })
-        .populate({ path: "details.additions.addition" });
-      return requestList;
-    } catch (error) {
-      throw error;
-    }
-  },
+    requests: async (args) => {
 
-  getDetailByRequestId: async (args) => {
-    try {
-      const { requestId } = args;
-      request = await Request.findOne({ _id: requestId }).populate({
-        path: "observations",
-        populate: "oservations",
-      });
+        let where = {};
+        if (args.filter != null && args.filter != undefined) {
+            where = helpers.getFilterFormObject(args.filter);
+        }
 
-      return request;
-    } catch (error) {
-      throw error;
-    }
-  },
+        let sort = { updatedAt: "asc" };
+        if (args.order != null && args.order != undefined) {
+            sort = helpers.getOrderFromObject(args.order);
+        }
 
-  getMyRequest: async (args) => {
-    try {
-      const { userId } = args,
-        requestList = await Request.find({ userId })
-          .populate({ path: "details.subproducto", model: "subproducts" })
-          .populate({ path: "details.additions.addition" });
-      return requestList;
-    } catch (error) {
-      throw error;
-    }
-  },
+        try {
+            const requestList = Request.find(where).sort(sort)
+                .populate({ path: 'userId', model: 'User' })
+                .populate({ path: 'details.subproducto', model: 'subproducts' })
+                .populate({ path: 'details.additions.addition' });
+            return requestList;
+        } catch (error) {
+            throw error;
+        }
+    },
 
-  createRequest: async (args) => {
-    try {
-      const requestInfo = args.request,
-        request = await Request.create({
-          ...requestInfo,
-          status: 1,
-          createdAt: new Date().toISOString(),
-          updatedAt: new Date().toISOString(),
-        });
-      return request;
-    } catch (error) {
-      throw error;
-    }
-  },
+    getDetailByRequestId: async (args) => {
+        try {
+            const { requestId } = args;
+            request = await Request.findOne({ _id: requestId }).populate({ path: 'observations', populate: 'oservations' });
 
-  updateRequest: async (args) => {
-    try {
-      const {
-        _id,
-        requestId,
-        productoId,
-        subproductoId,
-        userId,
-        latlng,
-      } = args.request;
-      const request = new Request({
-        _id,
-        requestId,
-        productoId,
-        subproductoId,
-        userId,
-        latlng,
-        updatedAt: new Date().toISOString(),
-      });
-      const newRequest = await Request.findOneAndUpdate(request._id, request);
-      if (!newRequest) {
-        throw new Error("Request not found");
-      }
-      return { ...newRequest._doc, _id: newRequest.id };
-    } catch (error) {
-      throw error;
-    }
-  },
-  acceptRequest: async (args) => {
-    try {
-      const { _id } = args.request;
+            return request;
+        } catch (error) {
+            throw error;
+        }
+    },
 
-      let request = await Request.findById(_id);
-      request = {
-        ...request._doc,
-        status: "2",
-        updatedAt: new Date(),
-      };
+    getMyRequest: async (args) => {
+        try {
+            const { userId } = args,
+                requestList = await Request.find({ userId })
+                    .populate({ path: 'details.subproducto', model: 'subproducts' })
+                    .populate({ path: 'details.additions.addition' });
+            return requestList;
+        } catch (error) {
+            throw error;
+        }
+    },
 
-      const newRequest = await Request.findByIdAndUpdate(request._id, request);
+    createRequest: async (args) => {
+        try {
+            //const currentDate =  helpers.getCurrentDateTime();
+            const requestInfo = args.request,
+                request = await Request.create({
+                    ...requestInfo,
+                    status: 1,
+                    createdAt: new Date().toISOString(),
+                    updatedAt: new Date().toISOString(),
+                });
+            return request;
+        } catch (error) {
+            throw error;
+        }
+    },
 
-      if (!newRequest) {
-        throw new Error("Request not found");
-      }
-      return { ...newRequest._doc, _id: newRequest._id };
-    } catch (error) {
-      throw error;
-    }
-  },
-  preparedRequest: async (args) => {
-    try {
-      const { _id } = args.request;
+    updateRequest: async (args) => {
+        try {
+            const {
+                _id,
+                requestId,
+                productoId,
+                subproductoId,
+                userId,
+                latlng,
+            } = args.request;
+            const request = new Request({
+                _id,
+                requestId,
+                productoId,
+                subproductoId,
+                userId,
+                latlng,
+                updatedAt: new Date().toISOString(),
+            });
+            const newRequest = await Request.findOneAndUpdate(
+                request._id,
+                request
+            );
+            if (!newRequest) {
+                throw new Error("Request not found");
+            }
+            return { ...newRequest._doc, _id: newRequest.id };
+        } catch (error) {
+            throw error;
+        }
+    },
 
-      let request = await Request.findById(_id);
+    cancelRequest: async (args) => {
+        try {
+            const { userId, _id, observations } = args.request;
+            const request = new Request({
+                _id: _id, //observations.requestId,
+                updatedAt: new Date().toISOString(),
+                status: 4,
+            });
+            console.log("QUE VIENE AQUÍ: ", _id);
+            console.log("Y QUE ES LO QUE VIENE AQUÍ: ", observations.requestId);
+            const newRequest = await Request.findOneAndUpdate(
+                { _id: { $eq: request._id } },
+                { $set: request },
+                { new: true, upsert: true }
+            );
+            console.log("Actualizó correctamente: ", newRequest);
+            const requestObs = new RequestObs({
+                requestId: request._id,
+                userId,
+                createdAt: new Date().toISOString(),
+                status: 4,
+                observation: observations.observation,
+            });
+            console.log(
+                "Observaciones que vienen en el request",
+                observations.observation
+            );
+            const newReqObs = await requestObs.save();
+            console.log("Nuevo requestObs: ", newReqObs);
+            const res = await Request.updateOne(
+                { _id: request._id },
+                {
+                    $push: { observations: newReqObs._id },
+                }
+            );
 
-      request = {
-        ...request._doc,
-        status: "4",
-        updatedAt: new Date(),
-      }
+            console.log("Respuesta del update: ", res);
 
-      const newRequest = await Request.findByIdAndUpdate(request._id, request);
+            if (!newRequest) {
+                throw new Error("Request not found");
+            }
 
-      if (!newRequest) {
-        throw new Error("Request not found");
-      }
-      return { ...newRequest._doc, _id: newRequest._id };
-    } catch (error) {
-      throw error;
-    }
-  },
-  deliveredRequest: async (args) => {
-    try {
-      const { _id } = args.request;
+            return { ...newRequest._doc, _id: newRequest._id };
+        } catch (error) {
+            throw error;
+        }
+    },
 
-      let request = await Request.findById(_id);
-
-      request = {
-        ...request._doc,
-        status: "5",
-        updatedAt: new Date(),
-      };
-
-      const newRequest = await Request.findByIdAndUpdate(request._id, request);
-
-      if (!newRequest) {
-        throw new Error("Request not found");
-      }
-      return { ...newRequest._doc, _id: newRequest._id };
-    } catch (error) {
-      throw error;
-    }
-  },
-  cancelRequest: async (args) => {
-    try {
-      const { _id } = args.request;
-
-      let request = await Request.findById(_id);
-
-      request = {
-        ...request._doc,
-        status: "3",
-        updatedAt: new Date(),
-      };
-
-      const newRequest = await Request.findByIdAndUpdate(request._id, request);
-      if (!newRequest) {
-        throw new Error("Request not found");
-      }
-      return { ...newRequest._doc, _id: newRequest._id };
-    } catch (error) {
-      throw error;
-    }
-  },
-
-  deleteRequest: async (args) => {
-    try {
-      const { _id } = args.request;
-      const request = new Request({
-        _id: _id,
-      });
-      const newRequest = await request.deleteOne(request._id);
-      return { ...newRequest._doc, _id: newRequest.id };
-    } catch (error) {
-      throw error;
-    }
-  },
+    deleteRequest: async (args) => {
+        try {
+            const { _id } = args.request;
+            console.log(args);
+            const request = new Request({
+                _id: _id,
+            });
+            const newRequest = await request.deleteOne(request._id);
+            return { ...newRequest._doc, _id: newRequest.id };
+        } catch (error) {
+            throw error;
+        }
+    },
 };
